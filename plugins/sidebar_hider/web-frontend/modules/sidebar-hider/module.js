ce567422966218b4cc283539405cdec042823b39
regan-stafflink <regan@stafflink.com.au>
2025-07-31 13:49:50 +1000

    wip

--- plugins/sidebar_hider/web-frontend/modules/sidebar-hider/module.js
+++ plugins/sidebar_hider/web-frontend/modules/sidebar-hider/module.js
@@ -1,4 +1,6 @@
-export const metadata = {
-  name: 'Stafflink Sidebar Hider',
-  version: '1.0.0',
+module.exports = function SidebarHiderModule(/* moduleOptions */) {
+  // You can register plugin files here if needed
+  this.nuxt.hook('ready', async (nuxt) => {
+    console.log('Sidebar Hider module loaded')
+  })
 }
